3c2a7dbf81f03146487038509c04cc2f5651c03d
physwizz <pollockg86@gmail.com>
2023-11-02 20:32:50 +1100

    fix makefile

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,5 +5,7 @@ export PLATFORM_VERSION=12
 export ANDROID_MAJOR_VERSION=s
 export ARCH=arm64
 
-make ARCH=arm64 exynos850-a21snsxx_defconfig
+make ARCH=arm64 physwizz_defconfig
 make ARCH=arm64 -j64
+
+
